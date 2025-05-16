90f776963172d541de39a8ef97e2bdc707527c3a
Alejandro Cebrián Sánchez <alejandrocebriansanchez@posteo.es>
2025-05-16 03:19:26 +0200

    Obstacles added

---
+++ Assets/_scripts/obstacles/SavePos.cs
@@ -0,0 +1,18 @@
+﻿/*
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SavePos : MonoBehaviour
+{
+	public Transform checkPoint;
+
+	void OnTriggerEnter(Collider col)
+	{
+		if (col.gameObject.tag == "Player")
+		{
+			col.gameObject.GetComponent<CharacterControls>().checkPoint = checkPoint.position;
+		}
+	}
+}
+*/
